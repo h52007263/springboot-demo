a1ce7d78e6dd0a846242f28493dc4dd2c2d54895
DennisHe <1569929533@qq.com>
2019-12-14 01:16:39 +0800

    添加第三个功能

--- github/demo/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ github/demo/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -16,6 +16,7 @@ public class HelloWorldController {
     public String index() {
         String str1 = "新增的分支上添加一个字符串";
         String str2 = "新增的分支上添加第二个字符串";
+        String str3 = "新增的分支上添加第二个字符串";
         return "Hello World";
     }
 }
